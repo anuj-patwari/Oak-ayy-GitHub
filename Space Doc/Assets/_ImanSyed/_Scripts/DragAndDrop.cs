0004825544517e0519994bb6a057143ce6452f57
Iman <31649603+ImanSyed@users.noreply.github.com>
2018-02-01 15:34:17 +0530

    PlanetRotation

---
+++ Space Doc/Assets/_ImanSyed/_Scripts/DragAndDrop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour {
+
+	private bool mouseDown;
+
+	void Update () {
+
+		if (mouseDown) {
+			float distance = Vector3.Distance (transform.position, Camera.main.transform.position);
+			Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Vector3 rayPoint = mouseRay.GetPoint (distance);
+			rayPoint.z = transform.position.z;
+			transform.position = rayPoint;
+		}
+	}
+
+	void OnMouseDown(){
+		mouseDown = true;
+	}
+
+	void OnMouseUp(){
+		mouseDown = false;
+	}
+		
+}
---
+++ Space Doc/Assets/_ImanSyed/_Scripts/RotationScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour {
+
+
+	public float sensitivity;
+	Vector3 mouseReference, mouseOffset, rot;
+	bool isRotating;
+
+	// Use this for initialization
+	void Start () {
+		rot = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		if (isRotating) {
+			mouseOffset = Input.mousePosition - mouseReference;
+			rot.y = -( mouseOffset.x) * sensitivity;
+			transform.Rotate (rot);
+			mouseReference = Input.mousePosition;
+		} else {
+			rot.y = -0.05f;
+			transform.Rotate (rot);
+		}
+	}
+
+	void OnMouseDown(){
+		isRotating = true;
+		mouseReference = Input.mousePosition;
+	}
+
+	void OnMouseUp(){
+		isRotating = false;
+	}
+}
