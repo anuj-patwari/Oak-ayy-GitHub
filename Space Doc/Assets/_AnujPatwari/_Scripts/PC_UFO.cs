c297d5bff610718ad14c52e2799532c6d8bada98
anujpatwari1804 <32254635+anujpatwari1804@users.noreply.github.com>
2018-02-01 15:31:45 +0530

    Pull Feature done

---
+++ Space Doc/Assets/_AnujPatwari/_Scripts/PC_UFO.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC_UFO : MonoBehaviour {
+
+	public float thrust;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent<Rigidbody2D> ();
+		rb.AddForce (transform.right * thrust);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+}
---
+++ Space Doc/Assets/_AnujPatwari/_Scripts/PullPlanetScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullPlanetScript : MonoBehaviour {
+
+	[SerializeField]
+	Rigidbody2D playerCharacter;
+
+	bool pcIsIn;
+
+	[SerializeField]
+	float force;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton (0) && pcIsIn) 
+		{
+			
+
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+			//Debug.DrawRay(ray.origin, ray.direction * 1000000, Color.green);
+			if (hit.collider != null) {
+				if (hit.collider.tag == "Finish") {
+					Vector2 forceDirection =  transform.position - playerCharacter.gameObject.transform.position;
+					playerCharacter.AddForce (forceDirection.normalized * force * Time.deltaTime);
+					Debug.Log (1);
+				}
+			}
+
+		}
+
+	}
+		
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+
+		if (col.tag == "Player") {
+
+			pcIsIn = true;
+		}
+
+	}
+
+	void OnTriggerExit2D (Collider2D col)
+	{
+
+		if (col.tag == "Player") {
+
+			pcIsIn = false;
+		}
+
+	}
+
+}
