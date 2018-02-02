6e93355ef33c76ad468beae265650bd4cac03e48
anujpatwari1804 <32254635+anujpatwari1804@users.noreply.github.com>
2018-02-02 12:17:23 +0530

    New features added
    Slow down area
    Meteor Spawning
    Restart on collision with meteor

---
+++ Space Doc/Assets/_AnujPatwari/_Scripts/SlowDownArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowDownArea : MonoBehaviour {
+
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+
+		if (col.tag == "Player") {
+
+			col.gameObject.GetComponent<Rigidbody2D> ().drag = 0.3f;
+
+		}
+
+	}
+}
