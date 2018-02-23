a8b4f7184b3075d1559105ba1f8d578d5f13c360
anujpatwari1804 <32254635+anujpatwari1804@users.noreply.github.com>
2018-02-23 16:19:54 +0530

    Pause Menu, Main Menu

--- Assets/_ImanSyed/_Scripts/GameManager.cs
+++ Assets/_ImanSyed/_Scripts/GameManager.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
+	public static bool gameIsPaused = false;
+	public GameObject pauseMenuUI;
+
+	[SerializeField]
+	string mainMenuScene;
+
 	public IEnumerator RestartAfter (float delay)
 	{
 		yield return new WaitForSeconds (delay);
@@ -13,5 +19,37 @@ public class GameManager : MonoBehaviour {
 
 	public void RestartInstant(){
 		SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+
+			if (gameIsPaused) {
+				Resume ();
+			} else 
+			{
+				Pause ();
+			}
+		}
+	}
+
+	public void Resume()
+	{
+		pauseMenuUI.SetActive (false);
+		Time.timeScale = 1f;
+		gameIsPaused = false;
+	}
+
+	public void Pause()
+	{
+		pauseMenuUI.SetActive (true);
+		Time.timeScale = 0f;
+		gameIsPaused = true;
+	}
+
+	public void MainMenu()
+	{
+		SceneManager.LoadScene (mainMenuScene);
 	}
 }
