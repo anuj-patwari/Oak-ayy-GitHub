b3a72f602683bdc4cdd94667fcd49e66d3b2b65f
anujpatwari1804 <32254635+anujpatwari1804@users.noreply.github.com>
2018-02-25 18:06:52 +0530

    main menu manager, game manager, pipe planet cubical

---
+++ Assets/_AnujPatwari/_Scripts/MainMenuManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour {
+
+	string mainMenuPlay;
+
+	[SerializeField]
+	Object playGame;
+
+	[SerializeField]
+	Sprite toggleSoundSprite, toggleOrigSoundSprite;
+
+	[SerializeField]
+	Image soundToggleButton;
+
+	// Use this for initialization
+	void Start () {
+
+		mainMenuPlay = playGame.name;
+		toggleOrigSoundSprite = soundToggleButton.sprite;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void toggleSoundSpriteFn()
+	{
+		if (soundToggleButton.sprite == toggleOrigSoundSprite) {
+			//when sound is on
+			soundToggleButton.sprite = toggleSoundSprite;
+		} else {
+			//when sound is off
+			soundToggleButton.sprite = toggleOrigSoundSprite;
+		}
+	}
+
+	public void MainMenuPlayButton()
+	{
+		SceneManager.LoadScene (mainMenuPlay);
+	}
+}
