e1901a8abd17d79df9ddda0be9290b8670a2f1e7
Iman <31649603+ImanSyed@users.noreply.github.com>
2018-04-06 15:27:41 +0530

    1.4

---
+++ Assets/_ImanSyed/_Scripts/TrailScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailScript : MonoBehaviour {
+
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Planet") {
+			Destroy (gameObject);
+		}
+	}
+}
