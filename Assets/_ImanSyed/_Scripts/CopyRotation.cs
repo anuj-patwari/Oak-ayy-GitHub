b01e715ab812fcf5d5df46cca5f071bf63b325ee
Iman <31649603+ImanSyed@users.noreply.github.com>
2018-04-30 15:36:37 +0530

    Fixed Animations

---
+++ Assets/_ImanSyed/_Scripts/CopyRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyRotation : MonoBehaviour {
+
+	[SerializeField]
+	Animator anim;
+
+	void Start () {
+		GetComponent<Animator> ().SetFloat ("Mult", anim.GetFloat ("Mult"));
+		Debug.Log (GetComponent<Animator> ().speed + " " + anim.speed);
+	}
+}
