c09907b733b8200bb37d99f2789e045ca92bddbb
Iman <31649603+ImanSyed@users.noreply.github.com>
2018-05-08 10:13:46 +0530

    aaaaaaaa

---
+++ Assets/_ImanSyed/_Scripts/ButtonPressEffect.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonPressEffect : MonoBehaviour, IPointerClickHandler {
+
+	public bool shrink;
+
+	[SerializeField]
+	float step = 0.05f;
+
+	float originalSize;
+	float smallSize;
+
+
+	void Start(){
+		originalSize = transform.localScale.x;
+		smallSize = transform.localScale.x / 2f;
+	}
+
+	public void OnPointerClick(PointerEventData data){
+		if (!shrink) {
+			if (transform.localScale.x >= originalSize) {
+				shrink = true;	
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (shrink) {
+			if (transform.localScale.x > smallSize) {
+
+				transform.localScale = Vector3.Lerp (transform.localScale, new Vector3 (smallSize - 0.05f, smallSize - 0.05f, smallSize - 0.05f), step);
+			} else {
+				transform.localScale = new Vector3 (smallSize, smallSize, smallSize);
+				shrink = false;
+			}
+		} else {
+			if (transform.localScale.x < originalSize) {
+				transform.localScale = Vector3.Lerp (transform.localScale, new Vector3 (originalSize + 0.05f, originalSize + 0.05f, originalSize + 0.05f), step);
+			} else {
+				transform.localScale = new Vector3 (originalSize, originalSize, originalSize);
+			}
+		}
+	}
+}
