9272586c411afadc11b5a5c5511eb38666354e6c
Iman <31649603+ImanSyed@users.noreply.github.com>
2018-05-13 14:53:19 +0530

    kekekekekekekekekekekekek

--- Assets/_ImanSyed/_Scripts/CutsceneManager.cs
+++ Assets/_ImanSyed/_Scripts/CutsceneManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 
@@ -9,9 +10,28 @@ public class CutsceneManager : MonoBehaviour {
 	[SerializeField]
 	string sceneToLoad;
 
+	GlobalGameManager ggm;
+
+	void Start(){
+		ggm = FindObjectOfType<GlobalGameManager> ();
+		ggm.as2.mute = true;
+		InvokeRepeating ("checkFrame", 0.5f, 0.5f);
+	}
+
 	void Update () {
 		if (Input.GetMouseButton (0)) {
+			ggm.as2.mute = false;
 			SceneManager.LoadScene (sceneToLoad);
 		}
 	}
+
+	void checkFrame(){
+		if (GetComponent<VideoPlayer> ().frame == (long)GetComponent<VideoPlayer> ().frameCount) {
+			ggm.as1.volume = 0;
+			ggm.as2.mute = false;
+			SceneManager.LoadScene (sceneToLoad);
+		}
+	}
+
+
 }
